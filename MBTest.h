1ef78ec9f46aa388b19d55f931b23862f4fe4e16
mattbuzz <mbuzby@gmail.com>
2014-02-04 20:20:25 -0800

    added a test class

---
+++ MBTest.h
@@ -0,0 +1,13 @@
+//
+//  MBTest.h
+//  Git Test
+//
+//  Created by Matt Buzby on 2/4/14.
+//  Copyright (c) 2014 Matt Buzby. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBTest : NSObject
+
+@end
---
+++ MBTest.m
@@ -0,0 +1,13 @@
+//
+//  MBTest.m
+//  Git Test
+//
+//  Created by Matt Buzby on 2/4/14.
+//  Copyright (c) 2014 Matt Buzby. All rights reserved.
+//
+
+#import "MBTest.h"
+
+@implementation MBTest
+
+@end
